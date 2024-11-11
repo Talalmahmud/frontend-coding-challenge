adb1f3b8eea692274d2d6bded52771a4dfe56040
Talal Mahmud <mahmudtalal2@gmail.com>
2024-11-11 11:46:20 +0600

    Fix: Implemented challenges

--- src/views/products/products.tsx
+++ src/views/products/products.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Product } from "@/types";
 import { ProductModal } from "@/views/products/productModal/productModal";
 import { BackToHome } from "@/components/backToHome/backToHome";
@@ -8,9 +8,13 @@ import { ProductList } from "@/views/products/productList/productList";
 import { PaginationControls } from "@/views/products/paginationControls/paginationControls";
 import { usePagination } from "@/hooks/usePagination";
 import { PRODUCTS_DATA } from "@/data/productsData";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export const Products: React.FC = () => {
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const router = useRouter();
+  const searchParmas = useSearchParams();
+
   const {
     currentPage,
     totalPages,
@@ -18,13 +22,33 @@ export const Products: React.FC = () => {
     handlePageChange,
   } = usePagination({ items: PRODUCTS_DATA, itemsPerPage: 5 });
 
-  const handleOpenModal = useCallback((product: Product) => {
-    setSelectedProduct(product);
-  }, []);
+  const handleOpenModal = useCallback(
+    (product: Product) => {
+      setSelectedProduct(product);
+      router.replace(`/products?product-id=${product?.id}`);
+    },
+    [router]
+  );
 
   const handleCloseModal = useCallback(() => {
     setSelectedProduct(null);
-  }, []);
+    router.replace(`/products`);
+  }, [router]);
+
+  const selectProductData = useCallback(() => {
+    const pId = searchParmas.get("product-id");
+    setSelectedProduct(
+      paginatedProducts?.find((item: Product | null) => item?.id === pId) ||
+        null
+    );
+  }, [searchParmas, paginatedProducts]);
+
+  useEffect(() => {
+    if (searchParmas.get("product-id")) {
+      console.log(paginatedProducts);
+      selectProductData();
+    }
+  }, [paginatedProducts, searchParmas, selectProductData]);
 
   return (
     <div>
